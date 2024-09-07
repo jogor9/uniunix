547c0a3abfafba4a4b0bfefcecafe25f62a6e9ba
JG <jgornas953@gmail.com>
2024-09-07 20:05:19 +0200

    cleanup

--- unihead/Main.hs
+++ unihead/Main.hs
@@ -1,31 +1,49 @@
-
-import qualified Text.Parsec as P
-import Data.List
-import qualified Data.ByteString.Lazy as BSL
 import Control.Monad
-import Data.Functor
-import System.IO
+import Data.ByteString.Lazy qualified as BSL
 import Data.Function
+import Data.Functor
+import Data.List
+import System.IO
+import Text.Parsec qualified as P
 import Uniunix
 
 parseNumber :: String -> Either String Integer
-parseNumber = ( P.optionMaybe (P.char '-')
-            <&> maybe id (const negate)   )
-            >>= (\neg -> neg . read <$> P.many1 P.digit)
-            >>= (\i -> P.optionMaybe suffixParser <&> maybe i (\f -> f i))
-            & parse'
+parseNumber =
+  P.option id (P.char '-' $> negate)
+    >>= (\neg -> neg . read <$> P.many1 P.digit)
+    >>= (\i -> P.optionMaybe suffixParser <&> maybe i (\f -> f i))
+    & parse'
 
-takeHead :: Integral i => (a -> i) -> (i -> a -> a) -> i -> a -> a
+takeHead :: (Integral i) => (a -> i) -> (i -> a -> a) -> i -> a -> a
 takeHead len take' i list =
-  if i >= 0 then
-    take' i list
-  else
-    take' (len list + i) list
+  if i >= 0
+    then
+      take' i list
+    else
+      take' (len list + i) list
 
 putFileHead :: OutputType -> FilePath -> IO ()
-putFileHead otype@(OBytes b) = getFileHandle otype >=> printFile (BSL.hGetContents) BSL.putStr (takeHead byteStringGenericLength byteStringGenericTake) b
-putFileHead otype@(OChars c) = getFileHandle otype >=> printFile hGetContents putStr (takeHead genericLength genericTake) c
-putFileHead otype@(OLines l) = getFileHandle otype >=> printFile (hGetContents >=> return . lines) (\ls -> putStr (unlines ls)) (takeHead genericLength genericTake) l
+putFileHead otype@(OBytes b) =
+  getFileHandle otype
+    >=> printFile
+      BSL.hGetContents
+      BSL.putStr
+      (takeHead byteStringGenericLength byteStringGenericTake)
+      b
+putFileHead otype@(OChars c) =
+  getFileHandle otype
+    >=> printFile
+      hGetContents
+      putStr
+      (takeHead genericLength genericTake)
+      c
+putFileHead otype@(OLines l) =
+  getFileHandle otype
+    >=> printFile
+      (hGetContents >=> return . lines)
+      (putStr . unlines)
+      (takeHead genericLength genericTake)
+      l
 putFileHead OUnknown = error "putFileHead should've never received OUnknown"
 
 main :: IO ()
